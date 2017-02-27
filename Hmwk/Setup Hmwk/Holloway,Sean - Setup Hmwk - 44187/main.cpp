2720085940618cbc434548d3ebfe627aaf96a418
sh2576104 <sholloway8@student.rcc.edu>
2017-02-26 20:26:05 -0800

    Setup Hmwk

---
+++ Hmwk/Setup Hmwk/Holloway,Sean - Setup Hmwk - 44187/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Sean Holloway
+ * Created on February 26, 2017, 6:00 PM
+ * Purpose:  Hello World Setup Homework
+
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    cout<<"Hello World"<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
+
